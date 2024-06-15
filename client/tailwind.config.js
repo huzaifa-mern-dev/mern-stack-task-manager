84691ca001b837f858d96d1c1e206a7bccf0c4bf
Muhammad Huzaifa <mhuzaifa3737@gmail.com>
2024-06-15 10:56:54 +0500

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,13 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-
-      colors: {
-        "primary-color": "var(--primary-color)",
-        "secondary-color": "var(--secondary-color)",
-        "first-text-color": "var(--text-color)",
-        "secondary-text-color": "var(--second-text-color)",
-      }
     },
   },
   plugins: [],
